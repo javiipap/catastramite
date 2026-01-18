c016529e460c5749eff24d4d1305e91a150e6cb0
javiipap <jpiopadilla@gmail.com>
2026-01-18 23:46:04 +0000

    feat: Add custom domain to web

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
 
+const ses = { actions: ["ses:*"], resources: ["*"] };
+
 export default $config({
   app(input) {
     return {
@@ -11,6 +13,13 @@ export default $config({
     };
   },
   async run() {
-    new sst.aws.Nextjs("MyWeb");
+    new sst.aws.Nextjs("CatastramiteWeb", {
+      permissions: [ses],
+      domain: {
+        name: "www.catastramite.com",
+        dns: false,
+        cert: "arn:aws:acm:us-east-1:372036281433:certificate/39f6af01-7e03-441c-b647-ac4cf178d405",
+      },
+    });
   },
 });
